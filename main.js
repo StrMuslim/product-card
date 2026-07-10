b6a11c5092ad7211339dfd9e43523d1a92ed6216
StrMuslim <Partobovohunso@gmail.com>
2026-07-10 14:54:51 +0500

    исправил ошибки

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
-const colorTheFirst = document.querySelector(
+const colorFirstCardBtn = document.querySelector(
   ".button-container__color-the-first-card",
 );
 
-const cardOne = document.querySelector(".card-one");
+const firstCard = document.querySelector(".card-one");
 
-const colorAll = document.querySelector(".button-container__color-all-cards");
+const colorAllCardsBtn = document.querySelector(".button-container__color-all-cards");
 
 const allCards = document.querySelectorAll(".card");
 
-colorTheFirst.addEventListener("click", function () {
-  if (cardOne.style.backgroundColor === "yellow") {
-    cardOne.style.backgroundColor = "";
-  } else {
-    cardOne.style.backgroundColor = "yellow";
-  }
+colorFirstCardBtn.addEventListener("click", function () {
+  firstCard.classList.toggle("yellow-background");
 });
 
-colorAll.addEventListener("click", function () {
-  allCards.forEach(function (card) {
-    if (card.style.backgroundColor === "tomato") {
-      card.style.backgroundColor = "";
-    } else {
-      card.style.backgroundColor = "tomato";
-    }
+colorAllCardsBtn.addEventListener("click", function () {
+  allCards.forEach(function(card) { 
+    card.classList.toggle("tomato-background");
   });
 });
 
 const title = document.querySelector(".title");
 
-title.addEventListener("click", function () {
+title.addEventListener("mouseup", function () {
   console.log(title);
 });
